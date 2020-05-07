3065d90c8ea8050cc6e3bdce32f1737dfb43462c
Remy Guts <remyguts@gmail.com>
2020-05-07 14:16:45 -0700

    writing render test and input test

--- client/src/components/Search/searchtest.js
+++ client/src/components/Search/searchtest.js
@@ -1,3 +1,18 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import { Search } from "./index";
+
+it("renders correctly", () => {
+  const { queryByTestId, queryByPlaceholderText } = render(<Search />);
+
+  expect(queryByTestId("button")).toBeTruthy();
+});
+
+describe("Input value", () => {
+  it("updates on change", () => {
+    const { queryByPlaceholderText } = render(<Search />);
+    const searchInput = queryByPlaceholderText("Search");
+    fireEvent.change(searchInput, { target: { value: "test" } });
+    expect(searchInput.value).toBe("test");
+  });
+});
