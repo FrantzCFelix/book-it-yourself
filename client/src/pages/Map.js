2eef5e4fef9123d9911bf8fcb547fcf704525a95
FrantzCFelix <Frantz.C.Felix@gmail.com>
2020-05-08 14:18:09 -0700

    changed from functional to class component

--- client/src/pages/Map.js
+++ client/src/pages/Map.js
@@ -1,25 +1,24 @@
-import React from "react";
-import Jumbotron from "react-bootstrap/Jumbotron";
+import React, { Component } from "react";
+import Map from "../components/MapComponent";
 import Nav from "../components/Nav";
-import MapTest from "../assets/images/mapTest.jpg";
 
-function Map() {
-  return (
-    <div>
-      <Nav />
-      <Jumbotron fluid>
-        <div>
-          <h5>Event Locator</h5>
-          <img
-            src={MapTest}
-            style={{ maxWidth: 375 }}
-            className="d-inline-block align-top"
-            alt="logo"
-          />
-        </div>
-      </Jumbotron>
-    </div>
-  );
+
+class MapPage extends Component {
+  constructor(props){
+    super(props);
+    this.state ={
+
+    };
+  }
+  render()
+  {
+    return (
+      <div>
+        <Nav />
+        <Map></Map>
+      </div>
+    );
+  }
 }
 
-export default Map;
+export default MapPage;
